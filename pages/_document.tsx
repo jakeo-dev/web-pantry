d13e0dc715e0454bf7edf945e692a15292d94d74
Jake <56329025+jakeo-dev@users.noreply.github.com>
2024-04-01 16:51:17 -0700

    Update Card and List views

---
+++ pages/_document.tsx
@@ -0,0 +1,13 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head />
+      <body className="px-3 md:px-8 py-16 md:py-20 transition-all">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
